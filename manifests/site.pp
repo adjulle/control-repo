cff297956c5cef7b9001cb56ac8f1d52825b45f0
Paul Talbot <adjulle@umich.edu>
2020-06-07 18:49:45 -0400

    classification cleanup

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,17 @@
-# Include default configuration for all nodes here
+node 'puppetmaster.vm.local' {
+  include role::master
+}
+
+node 'puppetdb.vm.local' {
+  include role::puppetdb
+}
+
+node 'puppetreports.vm.local' {
+  include role::puppetreports
+}
+
+node 'default' {
+  include role::agent
+
+  notify { 'Unclassified node': }
+}
