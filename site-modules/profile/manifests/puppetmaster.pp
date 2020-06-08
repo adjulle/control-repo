1eadd71b8c5e899f96a3c707672e40b3d7b87eb3
Paul Talbot <adjulle@umich.edu>
2020-06-08 13:37:11 -0400

    puppet class

--- site-modules/profile/manifests/puppetmaster.pp
+++ site-modules/profile/manifests/puppetmaster.pp
@@ -3,39 +3,55 @@
 
 class profile::puppetmaster(
     Boolean $use_puppetdb,
-    String $puppetdb_host,
+    String  $puppetdb_host,
+    Array   $autosign_hosts,
 ) {
 
-  class { 'puppet':
-    server => true,
-  }
-  contain 'puppetserver'
   # Ensure server starts before agent to avoid key issues
   # Service['puppetserver']->Service['puppet']
 
   $confdir = $::settings::confdir
   file { "${confdir}/autosign.conf":
     ensure  => file,
-    content => epp('profile/puppetmaster/autosign.conf.epp',{ 'autosign_hosts' => hiera('profiles::puppetmaster::autosign_hosts',[])}),
+    content => epp('profile/puppetmaster/autosign.conf.epp',{ 'autosign_hosts' => $autosign_hosts }),
   }
 
   class { '::puppetserver::hiera::eyaml':
     require => Class['puppetserver::install'],
   }
-  contain '::puppetserver::hiera::eyaml'
+  # contain '::puppetserver::hiera::eyaml'
 
   if $use_puppetdb {
 
-    class { 'puppetdb::master::config':
-      puppetdb_server             => $puppetdb_host,
-      manage_routes               => true,
-      manage_storeconfigs         => true,
-      manage_report_processor     => true,
-      enable_reports              => true,
-      strict_validation           => false,
-      puppetdb_soft_write_failure => true
+    class { 'puppet':
+      server              => true,
+      server_foreman      => false,
+      server_reports      => 'puppetdb',
+      server_storeconfigs => true,
     }
-    contain 'puppetdb::master::config'
 
+    class { 'puppet::server::puppetdb':
+      server => $puppetdb_host,
+    }
+
+    # class { 'puppetdb::master::config':
+    #   puppetdb_server             => $puppetdb_host,
+    #   manage_routes               => true,
+    #   manage_storeconfigs         => true,
+    #   manage_report_processor     => true,
+    #   enable_reports              => true,
+    #   strict_validation           => false,
+    #   puppetdb_soft_write_failure => true
+    # }
+    # contain 'puppetdb::master::config'
+
+  }
+  else {
+    class { '::puppet':
+      server                => true,
+      server_foreman        => false,
+      server_reports        => 'store',
+      server_external_nodes => '',
+    }
   }
 }
