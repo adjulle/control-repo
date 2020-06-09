ea29d136ac77483d80e6dd9aea51438e0315ee88
Paul Talbot <adjulle@umich.edu>
2020-06-09 12:05:40 -0400

    updates

--- site-modules/profile/manifests/puppetmaster_standalone.pp
+++ site-modules/profile/manifests/puppetmaster_standalone.pp
@@ -11,24 +11,26 @@ class profile::puppetmaster_standalone(
 
   include epel
 
-  yumrepo { 'Foreman':
-    ensure    => present,
-    assumeyes => true,
-    baseurl   => 'https://yum.theforeman.org/releases/2.1/el7/x86_64/',
-    descr     => 'Foreman',
-    enabled   => 1,
-    gpgcheck  => 0,
-  }
-
-  class { 'foreman':  }
-
-  # class { 'puppet':
-  #   server              => true,
-  #   server_ca           => true,
-  #   server_reports      => 'puppetdb',
-  #   server_storeconfigs => true,
-  #   autosign            => $autosign,
+  # yumrepo { 'Foreman':
+  #   ensure    => present,
+  #   assumeyes => true,
+  #   baseurl   => 'https://yum.theforeman.org/releases/2.1/el7/x86_64/',
+  #   descr     => 'Foreman',
+  #   enabled   => 1,
+  #   gpgcheck  => 0,
   # }
+
+  # class { 'foreman':  }
+
+  class { 'puppet':
+    server_puppetserver_version => '6.16.0',
+    server                      => true,
+    server_ca                   => true,
+    server_foreman              => false,
+    server_reports              => 'puppetdb',
+    server_storeconfigs         => true,
+    autosign                    => $autosign,
+  }
 
   # class { 'puppet::server::puppetdb':
   #   server => $puppetdb_host,
